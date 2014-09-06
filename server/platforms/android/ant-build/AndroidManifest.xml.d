3ed13f6e56a42b0468287ced356fccdca42b87ac
Rahman Qureshi <rahman.a.qureshi@gmail.com>
2014-09-06 10:10:32 -0400

    un-tested user pooling and web-app

---
+++ server/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+c:\cygwin\home\Rahman\mhacks\phonegap2\my-app\platforms\android\ant-build\AndroidManifest.xml : \
+c:\cygwin\home\Rahman\mhacks\phonegap2\my-app\platforms\android\AndroidManifest.xml \
