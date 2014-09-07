24b316f6c8d23d58a871418c4cfadc12f1abfabf
John Meade <jon.meadee@gmail.com>
2014-09-06 23:31:29 -0400

    cleaned up, more print bombing

--- server/www/js/controllers/MainCtrl.js
+++ server/www/js/controllers/MainCtrl.js
@@ -57,7 +57,6 @@ angular.module('geoChatApp')
         var name = prompt('Please enter your name');
         var radius = prompt('Please enter a radius');
         LocationService.getLocation().then(function (position) {
-            console.log(position);
             socket.emit('client:add_room', {
                 name: name,
                 position: {
--- server/www/js/services/LocationService.js
+++ server/www/js/services/LocationService.js
@@ -2,10 +2,24 @@ angular.module('geoChatApp')
     .factory('LocationService', function LocationService($q, $timeout) {
     return {
         getLocation: function () {
+            var dummy = {
+                    coords: {
+                        longitude: 5,
+                        latitude: 5
+                    }
+            };
 
             var deferred = $q.defer();
+            
+            $timeout(function () {
+                deferred.resolve(dummy);
+            }, 1000);
+
+            return deferred.promise;
+
+            /*
+            var deferred = $q.defer();
             navigator.geolocation.watchPosition(function (position) {
-                console.log(position);
                 deferred.resolve(position);
             }, function () {
                 deferred.reject('Timeout'); // is it $q.reject?
@@ -13,6 +27,7 @@ angular.module('geoChatApp')
                 timeout: 5000
             });
             return deferred.promise;
+            */
         }
     };
 
